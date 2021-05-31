0a4636e86c0a2dc7fb8c5e0bcbf839f69cff5e25
Zeqiang Li <mingmingmou@gmail.com>
2021-05-31 09:57:03 +0800

    actor interface

--- templates/common/Classes/Actor.h
+++ templates/common/Classes/Actor.h
@@ -5,17 +5,11 @@ class Actor {
 public:
     bool init(uint32_t modelID, cc::MODELTYPE modelType, cc::CLASSIFYTYPE subType);
 
-    //this will recreate the model with same modelID.
-    void updateModelType(cc::MODELTYPE type);
-
     void create(cc::Vec3 position, cc::Vec3 eulerAngle);
-    void createParking(cc::Vec3 position, cc::Vec3 eulerAngle);
-    void createInteractableObject(cc::Vec3 position, cc::Vec3 eulerAngle);
 
     void remove();
     void update(cc::Vec3 position, cc::Vec3 eulerAngle, float speed);
 
-    void changeTrack(cc::TRACKTYPE tracktype);
     void updateMaterialProperty(cc::MaterialProperty prop);
     // play animation at once, recall to play from begining again.
     void playAnimation();
